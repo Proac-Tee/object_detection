c535a9c4f08178921aeb47c57e547c1291c41ba6
Babatunde Taiwo <babatundetaiwo038@gmail.com>
2024-11-17 11:24:13 +0000

    feat: production config change

--- src/app/components/Detection.tsx
+++ src/app/components/Detection.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { InferenceSession, Tensor } from "onnxruntime-web";
 import { round } from "lodash";
-import { FC, useState } from "react";
+import {  useState } from "react";
 import { useEffect } from "react";
 import * as runModelUtils from "../utils/runModel";
 import ObjectDetectionCamera from "./ObjectDetectionCamera";
