7c97ee631df6c2eec12f029a2b29b5e417c7e2af
Babatunde Taiwo <babatundetaiwo038@gmail.com>
2024-11-23 12:44:19 +0000

    fix: search params suspense error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,44 @@
 "use client";
 
 import Dashboard from "@/components/Dashboard/Dashboard";
-import React from "react";
+import React, { Suspense } from "react";
 import Protected from "../utils/Protected";
+import Image from "next/image";
+import loading_image from "../assets/loading.svg";
 
 const DashboardPage = ({
   searchParams,
 }: {
   searchParams: Record<string, string>;
 }) => {
-  const page = searchParams["page"] ?? "1";
+  return (
+    <Suspense
+      fallback={
+        <div className="w-[100%] flex justify-center items-center max-w-[1440px] mx-auto p-4 ">
+          <Image
+            quality={100}
+            sizes="(min-width: 768px) 100vw, 700px"
+            src={loading_image}
+            alt="hero image"
+            className="w-8 h-8"
+          />
+        </div>
+      }
+    >
+      <DashboardWrapper searchParams={searchParams} />
+    </Suspense>
+  );
+};
+
+// Extract the logic to avoid issues with Suspense wrapping
+const DashboardWrapper = ({
+  searchParams,
+}: {
+  searchParams: Record<string, string>;
+}) => {
+  const page = parseInt(searchParams["page"] ?? "1", 10); // Ensure page is a number
   const per_page = 10;
+
   return (
     <section className="min-h-[90vh] w-[100%] h-[100%] bg-white">
       <Dashboard page={page} per_page={per_page} />
